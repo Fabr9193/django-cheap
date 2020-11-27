6581d4b71245d82821a5181a2cb1873c51798489
Fabricio Alegria <fabr9193@gmail.com>
2020-11-27 09:17:43 +0100

    adding relation user to flight

--- flights/models.py
+++ flights/models.py
@@ -1,10 +1,12 @@
 from djongo import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 # TODO mettre a  jour les datas pour refleter toutes les données qu'on peut récuperer/utiliser
 
 
 class Flight(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     departure = models.CharField(max_length=80)
     arrival = models.CharField(max_length=80)
     price = models.IntegerField()
